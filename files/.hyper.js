e66a1447424a81e489d5da52761e1d7aab5c5023
Reuben <reubendevries@gmail.com>
2024-03-16 15:07:21 -0700

    Updated hyper settings back to normal.
    Signed-off-by: Reuben <reubendevries@gmail.com>

--- files/.hyper.js
+++ files/.hyper.js
@@ -1,11 +1,14 @@
 "use strict";
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
 module.exports = {
     config: {
         // choose either `'stable'` for receiving highly polished,
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
-        fontSize: 14,
+        fontSize: 12,
         // font family with optional fallbacks
         fontFamily: 'MesloLGS NF',
         // default font weight: 'normal' or 'bold'
@@ -72,6 +75,23 @@ module.exports = {
             lightCoral: '#F08080',
         },
         // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+        // if left empty, your system's login shell will be used by default
+        //
+        // Windows
+        // - Make sure to use a full path if the binary name doesn't work
+        // - Remove `--login` in shellArgs
+        //
+        // Windows Subsystem for Linux (WSL) - previously Bash on Windows
+        // - Example: `C:\\Windows\\System32\\wsl.exe`
+        //
+        // Git-bash on Windows
+        // - Example: `C:\\Program Files\\Git\\bin\\bash.exe`
+        //
+        // PowerShell on Windows
+        // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+        //
+        // Cygwin
+        // - Example: `C:\\cygwin64\\bin\\bash.exe`
         shell: '',
         // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
         // by default `['--login']` will be used
@@ -109,26 +129,33 @@ module.exports = {
         screenReaderMode: false,
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
-        plugins: 
-            [
-                'hyper-material-theme',
-                'hyper-solarized-light', 
-                'hyper-night-and-day', 
-                'hyperpower', 'hyper-search', 
-                'hyper-tabs-enhanced', 
-                'hyper-quit', 
-                'hyper-highlight-active-pane', 
-                'hyper-single-instance', 
-                'hypercwd'
-            ],
-        // in development, you can create a directory under
-        // `~/.hyper_plugins/local/` and include it here
-        // to load it and avoid it being `npm install`ed
-        localPlugins: [],
-        keymaps: {
-            // Example
-            // 'window:devtools': 'cmd+alt+o',
-        },
-    }
+        // for advanced config flags please refer to https://hyper.is/#cfg
+    },
+    // a list of plugins to fetch and install from npm
+    // format: [@org/]project[#version]
+    // examples:
+    //   `hyperpower`
+    //   `@company/project`
+    //   `project#1.0.1`
+    plugins: 
+        [
+            'hyper-material-theme',
+            'hyper-solarized-light', 
+            'hyper-night-and-day', 
+            'hyperpower', 'hyper-search', 
+            'hyper-tabs-enhanced', 
+            'hyper-quit', 
+            'hyper-highlight-active-pane', 
+            'hyper-single-instance', 
+            'hypercwd'
+        ],
+    // in development, you can create a directory under
+    // `~/.hyper_plugins/local/` and include it here
+    // to load it and avoid it being `npm install`ed
+    localPlugins: [],
+    keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+    },
 };
 //# sourceMappingURL=config-default.js.map
