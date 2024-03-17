dc3bf8d5c4ae18f4687812b3a9b797dd9d6f54ae
Reuben <reubendevries@gmail.com>
2024-03-16 19:36:09 -0700

    revert
    Signed-off-by: Reuben <reubendevries@gmail.com>

--- files/.hyper.js
+++ files/.hyper.js
@@ -130,10 +130,6 @@ module.exports = {
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
         // for advanced config flags please refer to https://hyper.is/#cfg
-        dayNightSwitch: {
-            sunUp: 6,
-            sunDown: 18
-        },
     },
     // a list of plugins to fetch and install from npm
     // format: [@org/]project[#version]
@@ -141,21 +137,20 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: [	
+	    'hyperline',
+	    'hyperpower',
+	    'hyper-search',
+	    'hyper-tabs-enhanced',
+	    'hyper-quit',
+	    'hyper-highlight-active-pane',
+	    'hyper-single-instance',
+	    'hypercwd'
+    ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
-    localPlugins: [
-	'hyper-day-night-switch',
-	'hyperline',
-	'hyperpower',
-	'hyper-search',
-	'hyper-tabs-enhanced',
-	'hyper-quit',
-	'hyper-highlight-active-pane',
-	'hyper-single-instance',
-	'hypercwd'
-    ],
+    localPlugins: [],
     keymaps: {
     // Example
     // 'window:devtools': 'cmd+alt+o',
